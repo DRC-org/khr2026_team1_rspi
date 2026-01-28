6fe1a3e5d044c882a02f1e7f3f0f618284ed645f
Taiga Saito <me@taigasaito.org>
2026-01-28 15:31:49 +0900

    debug: topic の publish 間隔を長めにしてみる

--- src/robot_control/src/robot_control/ros2_node.py
+++ src/robot_control/src/robot_control/ros2_node.py
@@ -24,7 +24,8 @@ class RobotController(Node):
         self.m3508_cntl = M3508Controller()
 
         # コマンドを 50 ms ごとに送信する
-        self.create_timer(0.05, self.send_control_command)
+        # → デバッグで一時的に 1 秒に変更
+        self.create_timer(1, self.send_control_command)
 
         self.get_logger().info("Robot Controller Node initialized")
 
