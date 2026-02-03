3921c19dbb1292c675e4d50568758d15ae5c699f
Taiga Saito <me@taigasaito.org>
2026-02-03 18:41:41 +0900

    change: 最大 RPM を変更

--- src/robot_control/src/robot_control/constants.py
+++ src/robot_control/src/robot_control/constants.py
@@ -1,6 +1,6 @@
 import math
 
-M3508_MAX_RPM = 400  # モータの最大回転数 [rev/min]
+M3508_MAX_RPM = 4000  # モータの最大回転数 [rev/min]
 WHEEL_RADIUS = 0.04925  # タイヤの半径 [m]
 MAX_SPEED_MPS = WHEEL_RADIUS * 2 * math.pi * (M3508_MAX_RPM / 60)  # 最大速度 [m/s]
 
